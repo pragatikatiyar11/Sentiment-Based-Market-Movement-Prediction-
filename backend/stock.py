c1941c0a081461e463c40aa263e3dc5c7744d50f
Pragati Katiyar <144712498+pragatikatiyar11@users.noreply.github.com>
2026-04-10 01:36:48 +0530

    Update stock.py

--- backend/stock.py
+++ backend/stock.py
@@ -1,5 +1,20 @@
 import yfinance as yf
 
 def get_stock(symbol):
-    data = yf.download(symbol, period="5d")
-    return data.tail(1).to_dict()
+    data = yf.download(symbol, period="5d", interval="1d")
+    
+    latest = data.tail(1)
+    prev = data.tail(2).head(1)
+
+    if latest.empty or prev.empty:
+        return None
+
+    latest_close = float(latest["Close"].values[0])
+    prev_close = float(prev["Close"].values[0])
+
+    change = latest_close - prev_close
+
+    return {
+        "price": latest_close,
+        "change": change
+    }
